cba7d0bd653454cee2807dcede993d89a0b263d8
AryaJulianda <official.aryajulianda@gmail.com>
2024-02-10 22:11:42 +0700

    feat(modals): create form server modals and apply upload image_

---
+++ components/file-upload.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: String;
+  endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].fileUrl);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
